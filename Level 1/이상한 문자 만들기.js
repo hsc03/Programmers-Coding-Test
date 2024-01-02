e620a22835d20eb46c4dd77aeeb31c53f7949b74
Seongcheol Hwang <hsc0710@naver.com>
2024-01-02 17:48:52 +0900

    20240102

---
+++ Level 1/이상한 문자 만들기.js
@@ -0,0 +1,19 @@
+function solution(s) {
+    var answer = '';
+    var count = 0;
+
+    for (var i = 0; i < s.length; i++) {
+        if (s[i] == ' ') {
+            count = 0;
+            answer += ' ';
+        } else {
+            if (count % 2 == 0) {
+                answer += s[i].toUpperCase();
+            } else {
+                answer += s[i].toLowerCase();
+            }
+            count += 1;
+        }
+    }
+    return answer;
+}
