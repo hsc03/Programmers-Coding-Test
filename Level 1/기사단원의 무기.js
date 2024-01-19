52e526995385cb5f38c40ff6d62c4ecf8e33744a
Seongcheol Hwang <hsc0710@naver.com>
2024-01-19 15:41:43 +0900

    2024-01-19

---
+++ Level 1/기사단원의 무기.js
@@ -0,0 +1,22 @@
+function solution(number, limit, power) {
+    let answer = 0;
+
+    for (let i = 1; i <= number; i++) {
+        let cnt = 0;
+        for (let j = 1; j <= Math.sqrt(i); j++) {
+            if (i % j === 0) {
+                if (Math.pow(j, 2) === i) {
+                    cnt += 1;
+                } else {
+                    cnt += 2;
+                }
+            }
+        }
+        if (cnt <= limit) {
+            answer += cnt;
+        } else {
+            answer += power;
+        }
+    }
+    return answer;
+}
