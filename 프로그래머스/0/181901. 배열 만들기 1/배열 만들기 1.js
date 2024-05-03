e6a36b017f942939496fd2ae95ce01026065a809
Seongcheol Hwang <80443990+hsc03@users.noreply.github.com>
2024-05-03 19:30:57 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.js
@@ -0,0 +1,11 @@
+function solution(n, k) {
+    let answer = [];
+    for (let i = 1; i <= n; i++) {
+        if (i * k > n) {
+            break
+        }
+        answer.push(i * k);
+        
+    }
+    return answer;
+}
