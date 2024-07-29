c1d8d4d22e9d9fbae19fedfa3439cc9be9869e12
Seongcheol Hwang <80443990+hsc03@users.noreply.github.com>
2024-07-29 11:34:42 +0900

    [level 0] Title: 369게임, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.js
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -1,10 +1,12 @@
 function solution(order) {
     let answer = 0;
+    
     const strOrder = String(order);
     for (let i in strOrder) {
         if (strOrder[i] === '3' || strOrder[i] === '6' || strOrder[i] === '9') {
             answer += 1;
         }
     }
+    
     return answer;
 }
