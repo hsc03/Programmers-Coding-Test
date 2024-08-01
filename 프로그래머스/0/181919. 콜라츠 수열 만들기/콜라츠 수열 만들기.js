b0a9c770e09c25f7cfb6f46b08c8dd08e8389608
Seongcheol Hwang <80443990+hsc03@users.noreply.github.com>
2024-08-01 13:26:50 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 3.51 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,22 @@
+function solution(n) {
+    let answer = [];
+    
+    answer.push(n);
+    
+    while (true) {
+        if (n % 2 == 0) {
+            n /= 2;
+            answer.push(n);
+        }
+        else if (n % 2 !== 0) {
+            n = 3 * n + 1;
+            answer.push(n);
+        }
+        if (n === 1) {
+            break
+        }
+    }
+    console.log(answer);
+    
+    return answer;
+}
