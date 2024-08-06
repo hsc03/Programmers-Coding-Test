aabaa59abe7c13b7a254ebb6d9b4546ad07a1ec3
Seongcheol Hwang <80443990+hsc03@users.noreply.github.com>
2024-08-06 14:44:27 +0900

    [level 0] Title: 수 조작하기 2, Time: 27.82 ms, Memory: 47.3 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -6,17 +6,31 @@ function solution(numLog) {
             continue
         }
         value = numLog[i] - numLog[i-1];
-        if (value == 1) {
-            answer += 'w';
-        }
-        if (value == -1) {
-            answer += 's';
-        }
-        if (value == 10) {
-            answer += 'd';
-        }
-        if (value == -10) {
-            answer += 'a';
+        // if (value == 1) {
+        //     answer += 'w';
+        // }
+        // if (value == -1) {
+        //     answer += 's';
+        // }
+        // if (value == 10) {
+        //     answer += 'd';
+        // }
+        // if (value == -10) {
+        //     answer += 'a';
+        // }
+        switch (value) {
+            case 1:
+                answer += 'w';
+                break;
+            case -1:
+                answer += 's';
+                break;
+            case 10:
+                answer += 'd';
+                break;
+            case -10:
+                answer += 'a';
+                break;
         }
     }
     return answer;
