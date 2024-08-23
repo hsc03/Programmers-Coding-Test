c0f022d3573d78aff7fe1fa672e5a04b938ed6de
Seongcheol Hwang <80443990+hsc03@users.noreply.github.com>
2024-08-23 21:48:58 +0900

    [level 1] Title: 예산, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,12 @@
+function solution(d, budget) {
+    let answer = 0
+    d.sort((a, b) => a - b);
+    for (let i=0; i < d.length; i++) {
+        budget -= d[i];
+        if (budget >= 0) {
+            answer = i + 1;
+        }
+        
+    }
+    return answer;
+}
